9a69653bbd523e071dc396fa0ea8f150785463ba
Dorian Snyder <dastin1015@gmail.com>
2013-07-09 23:46:54 -0700

    d710: remove rild and libril

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,15 +32,11 @@ TARGET_KERNEL_CONFIG := cyanogenmod_d710_defconfig
 # Notification LED
 BOARD_HAS_LED_NOTIF := true
 
-# RIL
-BOARD_MOBILEDATA_INTERFACE_NAME := "ppp0"
-
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/d710/recovery/recovery_keys.c
 BOARD_UMS_LUNFILE := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/d710/include
 # assert
 TARGET_OTA_ASSERT_DEVICE := epic4gtouch,SPH-D710,d710,smdk4210,SPH-D710VMUB,SPH-D710BST
 
